b1471b02f63a9c4f93fed2e9c333ed46ad8df065
nateMJ <natemaritim@gmail.com>
2017-06-23 11:46:06 +0300

    RR|AM: Add initial login test

---
+++ test/components/Login/Login.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Login from '../../../src/components/Login/Login';
+
+describe('Login', () => {
+    it('Login renders login screen', () => {
+      const component = renderer.create(<Login />);
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
+});
