8501f5447856511a0ddd503d59454a383b2c22bd
nateMJ <natemaritim@gmail.com>
2017-06-27 10:53:46 +0300

    WIP test component behaviour with props

--- test/components/Layers/Layers.test.jsx
+++ test/components/Layers/Layers.test.jsx
@@ -3,12 +3,13 @@ import renderer from 'react-test-renderer';
 import Layers from '../../../src/components/Layers/Layers';
 
 describe('Layers', () => {
+  let layers;
+  beforeEach(() => {
+    layers = shallow(<Layers layers={['layer-1', 'layer-2', 'layer-3']} />);
+  });
   it('Layers renders indicators', () => {
-
-    const layers = shallow(<Layers layers={['name 1', 'name 2', 'name 3']} />);
-
-      const component = renderer.create(<Layers />);
-      const json = component.toJSON();
-      expect(json).toMatchSnapshot();
-    });
+    const component = renderer.create(<Layers />);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
 });
