09b1450a5776111cb5320907b13962321003749c
Roy Rutto <royrutto@gmail.com>
2017-06-30 16:07:14 +0300

    Require default

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ module.exports = {};
 const gisida = module.exports;
 
 gisida.version = require('../package.json').version;
-gisida.Map = require('./components/Map/Map');
-gisida.Dashboard = require('./Dashboard');
+gisida.Map = require('./components/Map/Map').default;
+gisida.Dashboard = require('./Dashboard').default;
