9322b2f76680fad33564b5ba6c9d674e83ad8b6d
nateMJ <natemaritim@gmail.com>
2017-06-30 11:27:19 +0300

    Refactor labels data coordinates

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -570,38 +570,38 @@ class Map extends React.Component {
   }
 
   addLabels(layer, data) {
-    const labels = [];
-
-    data.forEach((row) => {
-      const el = document.createElement('div');
-      el.className = `marker-label marker-label-${layer.id}-${this.props.mapId}`;
-      el.style.width = layer.labels.width;
-      el.style.height = layer.labels.height;
-      el.style['font-size'] = '12px';
-      el.style['font-weight'] = 'normal';
-      $(el).html(Mustache.render(layer.labels.label, row));
-      layer.labels.data.forEach((label) => {
-        if (label[layer.labels.join[0]] === row[layer.labels.join[1]]) {
-          labels.push({
-            el,
-            offset: layer.labels.offset,
-            coordinates: [label.longitude, label.latitude],
-          });
-        }
-      });
-    });
-
-    if (this.map.getZoom() > layer.labels.minZoom) {
-      labels.forEach((row) => {
-        new mapboxgl.Marker(row.el, {
-          offset: row.offset,
-        })
-          .setLngLat(row.coordinates)
-          .addTo(this.map);
-      });
-    }
-
     if (layer.labels && layer.labels.data && layer.labels.join) {
+      const labels = [];
+
+      data.forEach((row) => {
+        const el = document.createElement('div');
+        el.className = `marker-label marker-label-${layer.id}-${this.props.mapId}`;
+        el.style.width = layer.labels.width;
+        el.style.height = layer.labels.height;
+        el.style['font-size'] = '12px';
+        el.style['font-weight'] = 'normal';
+        $(el).html(Mustache.render(layer.labels.label, row));
+        layer.labels.data.forEach((label) => {
+          if (label[layer.labels.join[0]] === row[layer.labels.join[1]]) {
+            labels.push({
+              el,
+              offset: layer.labels.offset,
+              coordinates: [label[layer.labels.coordinates[0]], label[layer.labels.coordinates[1]]],
+            });
+          }
+        });
+      });
+
+      if (this.map.getZoom() > layer.labels.minZoom) {
+        labels.forEach((row) => {
+          new mapboxgl.Marker(row.el, {
+            offset: row.offset,
+          })
+            .setLngLat(row.coordinates)
+            .addTo(this.map);
+        });
+      }
+
       this.map.on('zoom', () => {
         if (this.map.getZoom() > layer.labels.minZoom) {
           this.removeLabels(layer);
