1eed53235638fcd982b6ad49a4ae5422ce2a190a
nateMJ <natemaritim@gmail.com>
2017-11-15 22:15:43 +0300

    Add funded by donor logos

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -23,16 +23,23 @@ class Login extends React.Component {
   render() {
     return (
       <div className="login">
-        <div>
-          <form className="login-form" onSubmit={(e) => { e.preventDefault(); this.handleLogin(this.password.value); }}>
-            <div className="form-group">
-              <div className={this.props.appConfig.defaultView === 'framework' ? 'framework-brand-login' : 'brand-login'} style={{ background: `url(${this.props.appConfig.loginIcon}) no-repeat center center` }} />
-              { this.state.loginError === true ? <div className="alert alert-danger">Incorrect password.</div> : null }
-              <label className="password-label" htmlFor="password">Enter your password</label>
-              <input className="form-control" type="password" ref={(input) => { this.password = input; }} autoFocus />
-            </div>
-            <button className="btn btn-default center-block" type="submit">Log In</button>
-          </form>
+        <form className="login-form" onSubmit={(e) => { e.preventDefault(); this.handleLogin(this.password.value); }}>
+          <div className="form-group">
+            <div className={this.props.appConfig.defaultView === 'framework' ? 'framework-brand-login' : 'brand-login'} style={{ background: `url(${this.props.appConfig.loginIcon}) no-repeat center center` }} />
+            { this.state.loginError === true ? <div className="alert alert-danger">Incorrect password.</div> : null }
+            <label className="password-label" htmlFor="password">Enter your password</label>
+            <input className="form-control" type="password" ref={(input) => { this.password = input; }} autoFocus />
+          </div>
+          <button className="btn btn-default center-block" type="submit">Log In</button>
+        </form>
+        <div className="funded-by">This dashboard is funded by:</div>
+        <div className="donor-logos">
+          <span className="donors">
+            <img src="/assets/img/dfid_logo.jpg" alt="DFID" />
+          </span>
+          <span className="donors">
+            <img src="/assets/img/danida_logo.png" alt="DANIDA" />
+          </span>
         </div>
       </div>);
   }
