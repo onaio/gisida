fbd8695abc811ced02c969b44d335dd587e19fca
cKellyDesign <ckelly@ona.io>
2018-04-26 22:40:55 -0400

    Fix lint error

--- src/map/prepareLayer.js
+++ src/map/prepareLayer.js
@@ -250,8 +250,7 @@ function fetchMultipleSources(mapId, layer, dispatch) {
     if (isManyToOne) {
       layerObj.joinedData = { ...mergedData };
       mergedData = Object.keys(mergedData).map(jd => ({ ...layerObj.joinedData[jd] }));
-        // .filter(jd => jd.reports.length);
-
+      // .filter(jd => jd.reports.length);
     }
 
     // convert to geojson format if necessary
