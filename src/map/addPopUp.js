d0658284faf99b24310f85e9575720b34caea307
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-06-05 17:41:11 +0300

    Change timeseriesLayer to use primaryLayerId property
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/map/addPopUp.js
+++ src/map/addPopUp.js
@@ -14,7 +14,7 @@ export default function addMousemoveEvent(mapId, mapboxGLMap, dispatch) {
   map.on('mousemove', (e) => {
     // Get layers from current state
     const currentState = dispatch(getCurrentState());
-    const { layers, timeseries, visibleLayerId } = currentState[mapId];
+    const { layers, timeseries, primaryLayer } = currentState[mapId];
 
     // Generate list of active layers
     const activeLayers = [];
@@ -49,7 +49,7 @@ export default function addMousemoveEvent(mapId, mapboxGLMap, dispatch) {
       let periodData = [];
       // Assign period data if layer has time series
       if (layer.aggregate && layer.aggregate.timeseries) {
-        const tsLayer = timeseries[visibleLayerId];
+        const tsLayer = timeseries[primaryLayer];
         if (tsLayer) {
           const currPeriod = Object.keys(tsLayer.periodData)[tsLayer.temporalIndex];
           periodData = tsLayer.periodData[currPeriod].data;
