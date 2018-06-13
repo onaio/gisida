701b592b421bbadf0905d21d50d847391119e948
Philipp Seitz <philippseitz2@gmail.com>
2018-06-13 13:58:56 -0400

    added missing tests for actions
    So far, may still need to define a few variables (these are commented)

--- test/store/actions/actions.test.js
+++ test/store/actions/actions.test.js
@@ -49,6 +49,17 @@ describe('actions', () => {
     expect(actions.addLayer(mapId, layer)).toEqual(expectedAction);
   });
 
+  //added by Philipp
+  test('should create an action to add layer group', () => {
+    //need to define groupId and group?
+    const expectedAction = {
+      type: types.ADD_LAYER_GROUP,
+      mapId,
+      groupId,
+      group
+    };
+    expect(actions.addLayerGroup(mapId, layer)).toEqual(expectedAction);
+  });
 
   test('should create an action to change region', () => {
     const region = 'Nairobi';
@@ -88,6 +99,40 @@ describe('actions', () => {
       mapId,
     };
     expect(actions.toggleFilter(mapId)).toEqual(expectedAction);
+  });
+
+  //added by Philipp
+  test('should create an action to set layer filter', () => {
+    //need to define layerFilters?
+    const expectedAction = {
+      type: types.SET_LAYER_FILTERS,
+      layerId,
+      layerFilters,
+      mapId,
+    };
+    expect(actions.setLayerFilter(mapId)).toEqual(expectedAction);
+  });
+
+  //added by Philipp
+  test('should create an action to update filters', () => {
+    const expectedAction = {
+      type: types.FILTERS_UPDATED,
+      layerId,
+      mapId,
+    };
+    expect(actions.filtersUpdated(mapId)).toEqual(expectedAction);
+  });
+
+  //added by Philipp
+  test('should create an action to save filter state', () => {
+    //need to define filterState?
+    const expectedAction = {
+      type: types.SAVE_FILTER_STATE,
+      layerId,
+      filterState,
+      mapId,
+    };
+    expect(actions.saveFilterState(mapId)).toEqual(expectedAction);
   });
 
   test('should create an action to update primary layer', () => {
@@ -156,6 +201,17 @@ describe('actions', () => {
       mapId,
     };
     expect(actions.updateTimeseries(mapId, timeseries)).toEqual(expectedAction);
+  });
+
+  //added by Philipp
+  test('should create an action to view in detail', () => {
+    //need to define payload?
+    const expectedAction = {
+      type: types.DETAIL_VIEW,
+      mapId,
+      payload,
+    };
+    expect(actions.detailView(mapId)).toEqual(expectedAction);
   });
 
   test('should create an action to turn off spinner', () => {
