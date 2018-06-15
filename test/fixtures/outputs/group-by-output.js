f03913d42026180ecff6318568a3f24acf969690
Philipp Seitz <philippseitz2@gmail.com>
2018-06-15 11:50:23 -0400

    Added groupBy util test

---
+++ test/fixtures/outputs/group-by-output.js
@@ -0,0 +1,38 @@
+const exOutputForGroupBy = 
+[{ //0
+	"OTP":
+	[{
+		"Type of site":"Hospital",
+		"Services":"OTP"
+	}],
+	"MCH TSP":
+	[{
+		"Type of site":"hospital",
+		"Services":"MCH TSP"
+	}],
+	"MCH Other":
+	[{
+		"Type of site":"Health post",
+		"Services":"MCH Other",
+		"Other Services":"Nutrition Counseling"
+	}]
+},
+{ //1
+	"OTP":
+	[{
+		"Type of site":"Hospital",
+		"Services":"OTP"
+	}],
+	"MCH Other":
+	[{
+		"Type of site":"hospital",
+		"Services":"MCH Other"
+	},
+	{
+		"Type of site":"Health post",
+		"Services":"MCH Other",
+		"Other Services":"Nutrition Counseling"
+	}]
+}];
+
+export default exOutputForGroupBy;
---
+++ test/utils/utils.test.groupBy.js
@@ -0,0 +1,18 @@
+import groupBy from '../../src/utils/groupBy.js/';
+
+//ex data to test on:
+import exInputForGroupBy from '../fixtures/group-by-input.js/';
+import exOutputForGroupBy from '../fixtures/outputs/group-by-output.js';
+
+describe('groupBy', () => {
+
+  test('Returns the correct object', () => {
+    expect(groupBy(exInputForGroupBy[0],'Services')).toEqual(exOutputForGroupBy[0]);
+  });
+
+  test('Returns the correct object', () => {
+    expect(groupBy(exInputForGroupBy[1],'Services')).toEqual(exOutputForGroupBy[1]);
+  });
+
+  
+})
