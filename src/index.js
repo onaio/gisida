58afebeba191e26d40ad0244639c5f43dfc5805a
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-10-29 15:46:11 +0300

    add parseData to module exports
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ gisida.hexToRgbA = require('./utils/hexToRgbA').default;
 gisida.parseMustache = require('./utils/parseMustache').default;
 gisida.getData = require('./connectors/ona-api/data').default;
 gisida.aggregateFormData = require('./connectors/ona-api/aggregateFormData').default;
+gisida.parseData = require('./utils/parseData').default;
 
 gisida.files = {};
 gisida.files.loadJSON = require('./utils/files').loadJSON;
