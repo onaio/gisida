657fbeecab9a84ed442b4cd9a3aaf1ae7988dad2
Kahama <pkahama@ona.io>
2018-12-06 15:39:55 +0300

    pass isOr param to aggregateformdata

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ gisida.hexToRgbA = require('./utils/hexToRgbA').default;
 gisida.parseMustache = require('./utils/parseMustache').default;
 gisida.getData = require('./connectors/ona-api/data').default;
 gisida.aggregateFormData = require('./connectors/ona-api/aggregateFormData').default;
+gisida.processFormData = require('./connectors/ona-api/aggregateFormData').processFormData;
 gisida.parseData = require('./utils/parseData').default;
 
 gisida.files = {};
