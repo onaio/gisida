4c96f9bd943c5eaf4534f357e9c76454d4f3a8aa
cKellyDesign <ckelly@ona.io>
2019-01-08 17:07:44 +0200

    Remove location control action

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -180,11 +180,6 @@ export function returnState(dispatch, getState) {
 
 export const getCurrentState = () => returnState;
 
-export const locationUpdated = (mapId) => ({
-  type: types.LOCATION_UPDATED,
-  mapId,
-});
-
 export default {
   initApp,
   initStyles,
