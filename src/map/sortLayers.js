a9fd516066bea54aed8e83641d47b471c02ec2b6
kahama <pkahama@ona.io>
2019-02-28 11:11:48 +0300

    include primarylayers when sorting layers

--- src/map/sortLayers.js
+++ src/map/sortLayers.js
@@ -1,32 +1,58 @@
-export default function sortLayers(map, layers) {
-  Object.keys(layers).forEach((key) => {
-    if (layers[key].type === 'line') {
-      if (map.getLayer(key)) {
-        map.moveLayer(key);
-      }
-    }
-  });
 
-  Object.keys(layers).forEach((key) => {
-    if (layers[key].type === 'circle') {
-      if (map.getLayer(key)) {
-        map.moveLayer(key);
+export default function sortLayers(map, layers, nextLayerId) {
+  /* Sort layers in the following order circle, symbol and detailview inclusive of primarylayer
+   -primary layer should always move on top for all layers in the respective categories
+   -the precedence can be viewed as shown below
+        1. detail-view primarylayer
+        2. detail-view (other detail-view layers that aren't primary layers)
+        3. symbol primarylayer
+        4. symbol (other symbol layers that aren't primary layers)
+        5. circle primarylayer
+        6. cirle (other circle layers that aren't primary layers)
+        7 line and fill layers primary
+        8. line and fill (other line and fill layers that aren't primary layers)
+  */
+  if (Object.keys(layers).length) {
+    const circles = Object.keys(layers).map(d => layers[d]).filter(d => d.type === 'circle'
+      && !d['detail-view']);
+    if (circles.length) {
+      Object.keys(circles).forEach((key) => {
+        if (circles[key].id !== nextLayerId) {
+          if (map.getLayer(circles[key].id)) {
+            map.moveLayer(circles[key].id);
+          }
+        }
+      });
+      if (circles.find(c => c.id === nextLayerId) && map.getLayer(nextLayerId)) {
+        map.moveLayer(nextLayerId);
       }
     }
-  });
+    const symbols = Object.keys(layers).map(d =>
+      layers[d]).filter(d =>
+      d.type === 'symbol' && !d['detail-view']);
 
-  Object.keys(layers).forEach((key) => {
-    if (layers[key].type === 'symbol') {
-      if (map.getLayer(key)) {
-        map.moveLayer(key);
+    if (symbols.length) {
+      Object.keys(symbols).forEach((key) => {
+        if (map.getLayer(symbols[key].id)) {
+          map.moveLayer(symbols[key].id);
+        }
+      });
+      if (symbols.find(s => s.id === nextLayerId) && map.getLayer(nextLayerId)) {
+        map.moveLayer(nextLayerId);
       }
     }
-  });
-  Object.keys(layers).forEach((key) => {
-    if (layers[key] && layers[key]['detail-view']) {
-      if (map.getLayer(key)) {
-        map.moveLayer(key);
+    const detailViewActive = Object.keys(layers).map(d =>
+      layers[d]).filter(d => d['detail-view']);
+
+    if (detailViewActive.length) {
+      Object.keys(detailViewActive).forEach((key) => {
+        if (map.getLayer(detailViewActive[key].id)) {
+          map.moveLayer(detailViewActive[key].id);
+        }
+      });
+      if (detailViewActive.find(d => d.id === nextLayerId) && map.getLayer(nextLayerId)) {
+        map.moveLayer(nextLayerId);
       }
     }
-  });
+  }
 }
