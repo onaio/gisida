ad02e8421d1af6952910f685ac4a2892c20da342
kahama <pkahama@ona.io>
2019-03-15 11:59:48 +0300

    working unstructured data sorting

--- src/map/generateStops.js
+++ src/map/generateStops.js
@@ -189,15 +189,15 @@ export default function (layer, timefield, dispatch, nextIndex) {
         if (!Number.isNaN(Date.parse((a.properties || a)[timefield]))) {
           return new Date((a.properties ||
              a)[timefield]) - new Date((b.properties || b)[timefield]);
+        }
+        else if (Number.isNaN(Date.parse((a.properties || a)[timefield]))) {
+              return new Date((a.properties ||
+                 a)[timefield].split('-')[0]) - new Date((b.properties || b)[timefield].split('-')[0]) ;
         } else if ((a.properties || a)[timefield] > (b.properties || b)[timefield]) {
           return 1;
         } else if ((b.properties || b)[timefield] > (a.properties || a)[timefield]) {
           return -1;
         } 
-        // else if (Number.isNaN(Date.parse((a.properties || a)[timefield]))) {
-        //       return new Date((a.properties ||
-        //          a)[timefield].split('-')[0]) - new Date((b.properties || b)[timefield].split('-')[0]) ;
-        // }
         return 0;
       });
     } 
