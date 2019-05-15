1eceb7289f2ccd6e948b565c08f9318ba9d76c6b
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-05-15 17:17:07 +0300

    Minor refactor
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/store/initStore.js
+++ src/store/initStore.js
@@ -127,11 +127,7 @@ export default function initStore(customReducers = {}, loadState, siteConfigUrl 
     );
   }
   // Create initial store
-  const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  const store = createStore(reducer, applyMiddleware(thunk));
 
   // Replace the store's reducer whenever a new reducer is registered.
   reducerRegistry.setChangeListener(reducers => store.replaceReducer(combine(reducers)));
