fba15b4c3de1d03c0b5c8a737a45c4a39c924661
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-07-10 14:49:00 +0300

    parse int periods to string
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/map/generateStops.js
+++ src/map/generateStops.js
@@ -191,10 +191,11 @@ export default function (layer, timefield, dispatch, nextIndex) {
       sortedData = rows.sort((a, b) => {
         if (!Number.isNaN(Date.parse((a.properties || a)[timefield]))) {
           return new Date((a.properties ||
-             a)[timefield]) - new Date((b.properties || b)[timefield]);
-        } else if (Number.isNaN(Date.parse((a.properties || a)[timefield]))) {
+            a)[timefield]) - new Date((b.properties || b)[timefield]);
+        } else if (Number.isNaN(Date.parse((a.properties || a)[timefield]))
+          && !Number.isNaN(Date.parse((a.properties || a)[timefield].toString().split('-')[0]))) {
           return new Date((a.properties ||
-                 a)[timefield].split('-')[0]) - new Date((b.properties || b)[timefield].split('-')[0]);
+            a)[timefield].toString().split('-')[0]) - new Date((b.properties || b)[timefield].toString().split('-')[0]);
         } else if ((a.properties || a)[timefield] > (b.properties || b)[timefield]) {
           return 1;
         } else if ((b.properties || b)[timefield] > (a.properties || a)[timefield]) {
