67029b7aab1db502ce8f6e46bcbd1ed1cfc0db6f
praveen-me <praveenkrsaini0@gmail.com>
2019-10-03 21:08:26 +0530

    replace XMLHttpRequest witrh fetch

--- src/connectors/ona-api/data.js
+++ src/connectors/ona-api/data.js
@@ -18,22 +18,24 @@ export default function getData(formID, properties, state, callback) {
     : (properties && properties[formID].map(p => `"${p}"`).join()) || null;
 
   const queryParams = fields && { fields: `[${fields}]` };
-  const xobj = new XMLHttpRequest();
   const mimeType = 'application/json';
   const activeHost = apiHost || host;
   const path = `${protocol}://${activeHost}/${endpoint}/${formID}.json${formatParams(queryParams)}`;
 
   try {
-    xobj.overrideMimeType(mimeType);
-    xobj.open('GET', path, true);
-    xobj.setRequestHeader('Authorization', `Token ${apiAccessToken}`);
-    xobj.onreadystatechange = () => {
-      if (xobj.readyState === 4 && xobj.status === 200) {
-        callback(null, JSON.parse(xobj.responseText));
+    fetch(path, {
+      method: 'GET',
+      headers: {
+        'content-type': mimeType,
+        Authorization: `Token ${apiAccessToken}`,
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        return res.json();
       }
-    };
-    xobj.send(null);
+      throw new Error(res.text());
+    }).then(data => callback(null, data));
   } catch (e) {
-    callback(e, xobj.responseText);
+    callback(e);
   }
 }
