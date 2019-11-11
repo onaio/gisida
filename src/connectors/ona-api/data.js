46f29ff19b438feef4ae5c65fba917b331fc9e30
kahummer <pkahama@ona.io>
2019-11-11 12:41:51 +0300

    Add try catch to fetch

--- src/connectors/ona-api/data.js
+++ src/connectors/ona-api/data.js
@@ -1,44 +1,53 @@
-const protocol = 'https';
-const host = 'api.ona.io';
-const endpoint = 'api/v1/data';
-
+const protocol = "https";
+const host = "api.ona.io";
+const endpoint = "api/v1/data";
 
 function formatParams(params) {
   if (!params) {
-    return '';
+    return "";
   }
   /** Build endpoint params */
-  const paramsList = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`);
-  return `?${paramsList.join('&')}`;
+  const paramsList = Object.keys(params).map(
+    key => `${key}=${encodeURIComponent(params[key])}`
+  );
+  return `?${paramsList.join("&")}`;
 }
 /** Method getData make's call to api takes in:
  * formID - the form id to pull data from.
  *
  */
 export default async function getData(formID, properties, state, callback) {
+  debugger;
   const { apiAccessToken, apiHost } = state;
-  const fields = properties && Array.isArray(properties)
-    ? properties.map(p => `"${p}"`).join()
-    : (properties && properties[formID].map(p => `"${p}"`).join()) || null;
+  const fields =
+    properties && Array.isArray(properties)
+      ? properties.map(p => `"${p}"`).join()
+      : (properties && properties[formID].map(p => `"${p}"`).join()) || null;
   const queryParams = fields && { fields: `[${fields}]` };
-  const mimeType = 'application/json';
+  const mimeType = "application/json";
   const activeHost = apiHost || host;
-  const path = `${protocol}://${activeHost}/${endpoint}/${formID}.json${formatParams(queryParams)}`;
+  const path = `${protocol}://${activeHost}/${endpoint}/${formID}.json${formatParams(
+    queryParams
+  )}`;
 
-  const response = await fetch(path, {
-    method: 'GET',
-    headers: {
-      Accept: mimeType,
-      Authorization: `Token ${apiAccessToken}`,
-    },
-  });
-  if (!response.ok) {
-    throw new Error(`form api failed, HTTP status ${response.status}`);
-  }
-  const data = await response.json();
-  if (data) {
-    callback(null, data);
-  } else {
-    throw new Error('form api failed, data not returned');
+  try {
+    fetch(path, {
+      method: "GET",
+      headers: {
+        "content-type": mimeType,
+        Authorization: `Token ${apiAccessToken}`
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        }
+        throw new Error(res.text);
+      })
+      .then(() => {
+        callback(null, data);
+      });
+  } catch (e) {
+    callback(e);
   }
 }
