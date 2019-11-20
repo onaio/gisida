e61188a2751ede16d6bfeba3f91d9d225f22eb2b
Kelvin Muchiri <kmubari@ona.io>
2019-11-20 14:57:27 +0300

    Add slice reducer for state primaryLayer

---
+++ src/store/reducers/createMapReducer/primaryLayerReducer.js
@@ -0,0 +1,46 @@
+import { TOGGLE_LAYER, UPDATE_PRIMARY_LAYER } from '../../constants/actionTypes';
+import defaultState from '../../defaultState';
+import { toggleLayer as toggleLayerLayersReducer } from './layersReducer';
+import { toggleLayer as toggleLayerActiveLayerIdsReducer } from './activeLayerIdsReducer';
+
+function toggleLayer(state, action) {
+  const { layerId } = action;
+  const layer = state.layers[layerId];
+  const activeLayerIds = toggleLayerActiveLayerIdsReducer(state, action);
+  const updatedLayers = toggleLayerLayersReducer(state.layers, action);
+
+  return {
+    ...state,
+    primaryLayer:
+      updatedLayers[layerId].visible && layer.credit
+        ? layer.id
+        : activeLayerIds[activeLayerIds.length - 1],
+  };
+}
+
+function updatePrimaryLayer(state, action) {
+  return {
+    ...state,
+    primaryLayer: action.primaryLayer,
+  };
+}
+
+export default function primaryLayerReducer(
+  state = {
+    primaryLayer: defaultState.MAP.primaryLayer,
+    layers: defaultState.MAP.layers,
+    activeLayerIds: defaultState.MAP.activeLayerIds,
+  },
+  action
+) {
+  switch (action.type) {
+    case TOGGLE_LAYER: {
+      return toggleLayer(state, action);
+    }
+    case UPDATE_PRIMARY_LAYER: {
+      return updatePrimaryLayer(state, action);
+    }
+    default:
+      return state;
+  }
+}
