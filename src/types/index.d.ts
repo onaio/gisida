7d9436ed4247e138b2ee880144b4a495aeb5fc37
ciremusyoka <wambua.eric01@gmail.com>
2019-12-17 15:40:55 +0300

    modified actions and their types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,18 @@
 declare module 'gisida' {
-    interface dispatch {
-        (arg:any):any
+
+    import { AnyAction } from 'redux';
+
+    interface generalFunc {
+        (...args: any[]):any
     }
+
+    interface generalObjects {
+        [key: string]: any
+    }
+
     const version:string;
     function initStore(customReducers:object, loadState:any, siteConfigUrl?:string): any;
-    function loadLayers(mapId:string, dispatch:any, layers:object):any
+    function loadLayers(mapId:string, dispatch:generalFunc, layers:object):any
     
     namespace reducerRegistry {
         function getReducers():object;
@@ -26,75 +34,71 @@ declare module 'gisida' {
     }
 
     namespace Actions {
-        function initApp(config:object):object;
-        function initLoc(config:object):object;
-        function initStyles(styles:Array<object>, mapConfig:object):object;
-        function initRegions(regions:any, mapConfig:object):object;
-        function initLocations(locations:object):object;
-        function initSuperset(config:object):object;
-        function addLayersList(layers:object):object;
-        function addLayer(mapId:string, layer:object):object;
-        function addLayerGroup(mapId:string, groupId:string, group:any):object;
-        function changeRegion(mapId:string, region:any):object;
-        function changeStyle(mapId:string, style:object):object;
-        function toggleLayer(mapId:string, layerId:string, isInit?:boolean):object;
-        function toggleFilter(mapId:string, layerId:string, showFilterPanel:boolean):object;
-        function setLayerFilter(mapId:string, layerId:string, layerFilters:object):object;
-        function filtersUpdated(mapId:string, layerId:string):object;
-        function saveFilterState(mapId:string, layerId:string, filterState:object, isClear:boolean):object;
-        function updatePrimaryLayer(mapId:string, primaryLayer:object):object;
-        function requestData(mapId:string, layerId:string):object;
-        function toggleMenu(mapId:string, menuIsOpen:boolean):object;
-        function toggleCategories(maId:string, category:any, index:number, isRefresh:boolean):object;
-        function receiveData(mapId:string, layer:object, timeseries:any):object;
-        function mapRendered(mapId:string, isRendered:boolean):object;
-        function mapLoaded(mapId:string, isLoaded:boolean):object;
-        function reloadLayer(mapid:string, layerId:string):object;
-        function reloadLayers(mapid:string, reload:any):object;
-        function layerReloaded(mapId:string):object;
-        function updateTimeseries(mapid:string, timeseries:any, layerId:string):object;
-        function detailView(mapid:string, payload:any):object;
-        function resetFilteredLayer(mapid:string, oldLayer:any):object;
-        function triggerSpinner(mapid:string, isLoaded?:boolean):object;
-        function returnState(dispatch:any, getState:any):object;
-        function loginRequest(credentials:object):object;
-        function loginSuccess(user:object):object;
-        function loginFailure(errorMessage:any):object;
+        function initApp(config:object):AnyAction;
+        function initLoc(config:object):AnyAction;
+        function initStyles(styles:Array<object>, mapConfig:object):AnyAction;
+        function initRegions(regions:any, mapConfig:object):AnyAction;
+        function initLocations(locations:object):AnyAction;
+        function initSuperset(config:object):AnyAction;
+        function addLayersList(layers:object):AnyAction;
+        function addLayer(mapId:string, layer:object):AnyAction;
+        function addLayerGroup(mapId:string, groupId:string, group:any):AnyAction;
+        function changeRegion(mapId:string, region:any):AnyAction;
+        function changeStyle(mapId:string, style:object):AnyAction;
+        function toggleLayer(mapId:string, layerId:string, isInit?:boolean):AnyAction;
+        function toggleFilter(mapId:string, layerId:string, showFilterPanel:boolean):AnyAction;
+        function setLayerFilter(mapId:string, layerId:string, layerFilters:object):AnyAction;
+        function filtersUpdated(mapId:string, layerId:string):AnyAction;
+        function saveFilterState(mapId:string, layerId:string, filterState:object, isClear:boolean):AnyAction;
+        function updatePrimaryLayer(mapId:string, primaryLayer:object):AnyAction;
+        function requestData(mapId:string, layerId:string):AnyAction;
+        function toggleMenu(mapId:string, menuIsOpen:boolean):AnyAction;
+        function toggleCategories(maId:string, category:any, index:number, isRefresh:boolean):AnyAction;
+        function receiveData(mapId:string, layer:object, timeseries:any):AnyAction;
+        function mapRendered(mapId:string, isRendered:boolean):AnyAction;
+        function mapLoaded(mapId:string, isLoaded:boolean):AnyAction;
+        function reloadLayer(mapid:string, layerId:string):AnyAction;
+        function reloadLayers(mapid:string, reload:any):AnyAction;
+        function layerReloaded(mapId:string):AnyAction;
+        function updateTimeseries(mapid:string, timeseries:any, layerId:string):AnyAction;
+        function detailView(mapid:string, payload:any):AnyAction;
+        function resetFilteredLayer(mapid:string, oldLayer:any):AnyAction;
+        function triggerSpinner(mapid:string, isLoaded?:boolean):AnyAction;
+        function loginRequest(credentials:object):AnyAction;
+        function loginSuccess(user:generalObjects):AnyAction;
+        function loginFailure(errorMessage:any):AnyAction;
         function getCurrentState():any;
-        function initAuth(config:object):object;
-        function requestLogin(creds:object):object;
-        function receiveLogin(user:object):object;
-        function loginError(message:any):object;
-        function receiveLogout():object;
-        function receiveToken(token:any):object;
-        function getAuthConfigs(config:object):object;
-        function receiveForms(forms:any):object;
-        function fetchFormsError(message:any):object;
-        function loginUser(token:string):object;
-        function getUserForms(token:string):object;
-        function logoutUser():object;
-        function locationUpdated(MapId:string):object;
-        function setLocation(mapId:string, loc:any):object;
-        function toggleMapLocation(loc:any):object;
+        function initAuth(config:object):AnyAction;
+        function receiveLogin(user:generalObjects):AnyAction;
+        function loginError(message:any):AnyAction;
+        function receiveToken(token:string):AnyAction;
+        function getAuthConfigs(config:object):AnyAction;
+        function receiveForms(forms:any):AnyAction;
+        function fetchFormsError(message:any):AnyAction;
+        function loginUser(token:string):AnyAction;
+        function logoutUser():AnyAction;
+        function locationUpdated(MapId:string):AnyAction;
+        function setLocation(mapId:string, loc:any):AnyAction;
+        function toggleMapLocation(loc:any):AnyAction;
         
     }
 
-    function prepareLayer(mapId:string, layer:object, dispatch:any, filterOptions:boolean, doUpdateTsLayer:boolean):any;
-    function addPopUp(mapId:string, mapboxGLdetailViewMap:any, dispatch:any):boolean;
+    function prepareLayer(mapId:string, layer:object, dispatch:generalFunc, filterOptions:boolean, doUpdateTsLayer:boolean):any;
+    function addPopUp(mapId:string, mapboxGLdetailViewMap:any, dispatch:generalFunc):boolean;
     function sortLayers(map:object, layers:object, nextLayerId:string):any;
     function getSliderLayers(layers:object):Array<any>;
     function buildFilters(filters:object,layerFilters:Array<any>, prevFilters:object):object;
     function mergeFilters(originalFilters:object, filteredFilters:object, clickedFilterKey:string):object;
     function generateFilterOptions(layerData:object):any
     function processFilters(layerData:object, filterOptions:object, isOr:boolean):any;
-    function generateStops(layer:object, timefield:Date, dispatch:any, nextIndex:any):any;
+    function generateStops(layer:object, timefield:Date, dispatch:generalFunc, nextIndex:any):any;
     function formatNum(num:any, decimal:number):string;
     function hexToRgbA(hex:any, alpha:any):any;
     function parseMustache(spec:any, datun:any):any;
 
     namespace SupAuth {
         function defaultOauthC():any;
-        function defaultSupAuthC(User:object, AuthConfig:object):any
+        function defaultSupAuthC(User:generalObjects, AuthConfig:object):any
         function defaultUnSupAuthZ():any;
         function authorizeUser(APP:object, AUTH:object, accessToken:string, willAuthorize?:boolean):any;
         function getUser():any;
@@ -110,19 +114,19 @@ declare module 'gisida' {
         Oauth2:any;
     }
 
-    function onaApi(config:object, callback:any):any;
+    function onaApi(config:object, callback:generalFunc):any;
 
     namespace onaAuth {
-        function oauthURL(clientID:string, callback:string, baseURL:string):string;
-        function ONAoauth(reqConfig:any, token:string, dispatch:any):any;
+        function oauthURL(clientID:string, callback:generalFunc, baseURL:string):string;
+        function ONAoauth(reqConfig:any, token:string, dispatch:generalFunc):any;
     }
 
-    function getData(formID:number, properties:Array<any>, state:object, callback:any):any;
+    function getData(formID:number, properties:Array<any>, state:object, callback:generalFunc):any;
     function aggregateFormData(layerData:object, locations:object, filterOptions:object, isOr:boolean):Array<any>;
     function parseData(spec:object,datum:Array<any>):Array<any>
     namespace history {}
 
-    function oauthURL(clientID:string, callback:string, baseURL:string):string;
+    function oauthURL(clientID:string, callback:generalFunc, baseURL:string):string;
     function buildParsedBasicDetailItem(detail:object, properties:object):object;
     function processFormData(formData:any, layerObj:object):any;
     function lngLat(LOC:object, APP:Object):object;
@@ -133,14 +137,14 @@ declare module 'gisida' {
     }
 
     namespace files {
-        function loadJSON(path:string, callback:any, id:any):any
-        function loadCSV(path:string, callback:any):any;
+        function loadJSON(path:string, callback:generalFunc, id:any):any
+        function loadCSV(path:string, callback:generalFunc):any;
     }
 
     function addChart(layer:object, data:Array<any>, map:any, mapId:string):any;
-    function buildDetailView(mapId:string, LayerObj:object, FeatureProperties:object, dispatch:any, timeSeriesObj:object):boolean;
-    function buildFilterState(mapId:string, filterOptions:object, filters:object, layerObj:object, dispatch:any, regenStops:object, isOr:boolean):object;
-    function clearFilterState(mapId:string, filterState:any, layerId:string, dispatch:any, isClear:boolean):any;
+    function buildDetailView(mapId:string, LayerObj:object, FeatureProperties:object, dispatch:generalFunc, timeSeriesObj:object):boolean;
+    function buildFilterState(mapId:string, filterOptions:object, filters:object, layerObj:object, dispatch:generalFunc, regenStops:object, isOr:boolean):object;
+    function clearFilterState(mapId:string, filterState:any, layerId:string, dispatch:generalFunc, isClear:boolean):any;
 
     namespace localStorage {
         function loadState():any;
