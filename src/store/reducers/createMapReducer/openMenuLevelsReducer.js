930fc186965243e85b39d5f85c97f7d1896023c4
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-01-08 16:49:43 +0300

    Add doc strings for slice reducer openMenuLevelsReducer

--- src/store/reducers/createMapReducer/openMenuLevelsReducer.js
+++ src/store/reducers/createMapReducer/openMenuLevelsReducer.js
@@ -1,5 +1,11 @@
 import { TOGGLE_CATEGORIES, TOGGLE_GROUPS } from '../../constants/actionTypes';
 
+/**
+ * Toggle open and close status of menu categories and groups levels
+ * @param {array} state - An array of categories or groups
+ * @param {*} action - Action to update the state
+ * @returns {array} - Updated state
+ */
 function toggleLevel(state, action) {
   const { category, group, index, isRefresh } = action;
   const level = category || group;
@@ -18,6 +24,12 @@ function toggleLevel(state, action) {
   return openLevels;
 }
 
+/**
+ * Slice reducer to handle the state of open menu categories and groups
+ * @param {array} state The current state of the reducer
+ * @param {*} action The dispatched action used to set the state of the reducer
+ * @returns {array} The updated state of the reducer
+ */
 export default function openMenuLevelsReducer(state = [], action) {
   switch (action.type) {
     case TOGGLE_CATEGORIES: {
