d4e16ee8a104a7e63291ffdcd2bde95f7560b3bf
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-01-10 14:15:28 +0300

    Associate an open menu group with its count.
    Store open groups as an array of objects rather than an array of
    strings. Each object has a count and group key. Prevents groups with
    the same name from being treated as one, toggling all if one is toggled.

--- src/store/reducers/createMapReducer/openGroupsReducer.js
+++ src/store/reducers/createMapReducer/openGroupsReducer.js
@@ -2,12 +2,15 @@ import { TOGGLE_GROUPS } from '../../constants/actionTypes';
 import defaultState from '../../defaultState';
 
 function toggleGroups(groupState, action) {
-  const { group, index, isRefresh } = action;
+  const { group, index, isRefresh, count } = action;
   const openGroups = [...groupState];
   if (index > -1) {
     openGroups.splice(index, 1);
   } else {
-    openGroups.push(group);
+    openGroups.push({
+      count,
+      group,
+    });
   }
   return isRefresh ? [] : [...openGroups];
 }
