6ddb6beee6ffa3d43b2b961d9285838faf2415f3
kahummer <pkahama@ona.io>
2020-01-28 19:17:17 +0300

    Mock wip

--- test/connectors/data.test.js
+++ test/connectors/data.test.js
@@ -6,12 +6,30 @@ const params = {
 };
 describe('Test formatParams method', () => {
   test('Should return null', () => {
-    expect(formatParams()).toEqual('');
+      expect(formatParams()).toEqual('');
   });
   test('Should return params list', () => {
-    expect(formatParams(params)).toEqual('?country=Philippines&name=map');
+      expect(formatParams(params)).toEqual('?country=Philippines&name=map');
   });
 });
 
 describe('Test getData method', () => {
+  //params needed for the
+  const callback = (error, Data) => {
+      if (error) {
+          return error;
+      } else if (Data) {
+          return Data
+      } else {
+          return null
+      }
+  }
+  const formID = 448373;
+  const properties = null;
+  const state = {
+      apiAccessToken: 'abc',
+      apiHost: 'api.ona.io'
+  }
+
+  getData(formID, properties, state, callback)
 });
