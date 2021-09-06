acd86d610d1bab1cefebe284eaa83c9064113dd6
kahummer <pkahama@ona.io>
2021-09-06 09:43:49 +0300

    Add Hapi/joi validator to commaFormating

--- src/utils/commaFormatting.js
+++ src/utils/commaFormatting.js
@@ -1,10 +1,35 @@
+const Joi = require('@hapi/joi');
+
 /**
- * 
- * @param {Object} layerObj layer specification/config object 
- * @param {Object} data data to be formatted a label/popup 
+ *
+ * @param {Object} layerObj layer specification/config object
+ * @param {Object} data data to be formatted a label/popup
  * @param {boolean} popup flag to indicate if call is coming from popup or not
  */
 export default function commaFormatting(layerObj, data, popup) {
+  const joiSchema = Joi.object({
+    dataMap: Joi.object()
+      .required()
+      .messages({
+        'object.base': `"a" should be a type of 'object'`,
+        'any.required': `"a" is a required field`,
+      }),
+    popupFlag: Joi.boolean()
+      .required()
+      .messages({
+        'boolean.base': `"a" should be a type of 'boolean'`,
+        'any.required': `"a" is a required field`,
+      }),
+  });
+
+  const validationResult = joiSchema.validate(
+    { dataMap: {}, popupFlag: false },
+    { abortEarly: false }
+  );
+  if (validationResult.error) {
+    alert(validationResult.error);
+  }
+
   const str = popup
     ? layerObj.popup && layerObj.popup.body
     : layerObj.labels && layerObj.labels.label;
@@ -13,7 +38,8 @@ export default function commaFormatting(layerObj, data, popup) {
   const rxp = /{{([^}]+)}/g;
   let foundZero = false;
   // Get flags to show/hide zero values on labels and popup
-  const showZerosOnLabels = layerObj.labels && layerObj.labels.showZeros !== undefined ? layerObj.labels.showZeros : true;
+  const showZerosOnLabels =
+    layerObj.labels && layerObj.labels.showZeros !== undefined ? layerObj.labels.showZeros : true;
 
   for (let c = rxp.exec(str); c !== null; c = rxp.exec(str)) {
     found.push(c[1]);
